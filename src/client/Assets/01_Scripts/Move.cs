a50ebd30385d9660624381d0f11ee9026b11b75e
kwak1s1h <kwak1s1h@kakao.com>
2022-09-13 09:14:39 +0900

    init

---
+++ src/client/Assets/01_Scripts/Move.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Experimental.Rendering.Universal;
+
+public class Move : MonoBehaviour
+{
+    private Rigidbody2D rigidbody2d;
+    [SerializeField] private float speed;
+
+    private Camera cam;
+    [SerializeField] private Light2D light2D;
+    [SerializeField] private GameObject floor;
+
+    private void Awake()
+    {
+        cam = Camera.main;
+        rigidbody2d = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        rigidbody2d.velocity = (new Vector2(Input.GetAxis("Horizontal"), 0)) * speed;
+        floor.transform.position = new Vector2(transform.position.x, floor.transform.position.y);
+        light2D.transform.position = new Vector2(transform.position.x, light2D.transform.position.y);
+        cam.transform.position = new Vector3(transform.position.x, cam.transform.position.y, -10);
+    }
+}
