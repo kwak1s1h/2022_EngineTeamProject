b60e48d3628987edb36f04b57cd822cd4a4fe17c
kwak1s1h <kwak1s1h@kakao.com>
2022-09-13 20:33:26 +0900

    다 죽어 그냥

--- src/client/Assets/01_Scripts/Move.cs
+++ src/client/Assets/01_Scripts/Move.cs
@@ -8,13 +8,11 @@ public class Move : MonoBehaviour
     private Rigidbody2D rigidbody2d;
     [SerializeField] private float speed;
 
-    private Camera cam;
     [SerializeField] private Light2D light2D;
     [SerializeField] private GameObject floor;
 
     private void Awake()
     {
-        cam = Camera.main;
         rigidbody2d = GetComponent<Rigidbody2D>();
     }
 
@@ -25,6 +23,5 @@ public class Move : MonoBehaviour
         // 조명, 바닥, 카메라 이동
         floor.transform.position = new Vector2(transform.position.x, floor.transform.position.y);
         light2D.transform.position = new Vector2(transform.position.x, light2D.transform.position.y);
-        cam.transform.position = new Vector3(transform.position.x, cam.transform.position.y, -10);
     }
 }
