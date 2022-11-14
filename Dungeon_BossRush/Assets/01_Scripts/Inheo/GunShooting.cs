a32d3388dbbd4c3c436665596d5154b628bd43e4
Inheoseongbin <seongbin529@gmail.com>
2022-11-14 16:42:21 +0900

    flip

--- Dungeon_BossRush/Assets/01_Scripts/Inheo/GunShooting.cs
+++ Dungeon_BossRush/Assets/01_Scripts/Inheo/GunShooting.cs
@@ -8,13 +8,29 @@ public class GunShooting : MonoBehaviour
     [SerializeField] private GameObject _bullet;
     [SerializeField] private Transform _pos;
     [SerializeField] private float _coolTime;
+
     private float _curTime;
+
+    [SerializeField] private SpriteRenderer _renderer;
+
+    Vector3 flipMove = Vector3.zero;
 
     void Update()
     {
         Vector2 len = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
         float z = Mathf.Atan2(len.y, len.x) * Mathf.Rad2Deg;
         transform.rotation = Quaternion.Euler(0, 0, z);
+        if(z > 90 || z < -90)
+        {
+            flipMove = Vector3.left;
+            _renderer.flipY = true;
+        }
+        else if (z < 90 || z > -90)
+        {
+            flipMove = Vector3.right;
+            _renderer.flipY = false;
+        }
+
         if (_curTime <= 0)
         {
             if (Input.GetMouseButton(0))
