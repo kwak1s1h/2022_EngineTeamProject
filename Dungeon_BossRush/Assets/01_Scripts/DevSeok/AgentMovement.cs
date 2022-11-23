fd441176c7272d727cf973a79ec64acb72a7f779
Inheoseongbin <seongbin529@gmail.com>
2022-11-23 10:18:08 +0900

    Nice

--- Dungeon_BossRush/Assets/01_Scripts/DevSeok/AgentMovement.cs
+++ Dungeon_BossRush/Assets/01_Scripts/DevSeok/AgentMovement.cs
@@ -8,6 +8,16 @@ public class AgentMovement : MonoBehaviour
     private Rigidbody2D _rigid;
     [SerializeField] private float _speed;
 
+    [SerializeField] private SpriteRenderer _playerRenderer;
+
+    private bool canDash = true;
+    private bool isDashing;
+    private float dashPower = 24f;
+    private float dashTime = 0.5f;
+    private float dashCoolTime = 1f;
+
+    [SerializeField] private TrailRenderer tr;
+
     private void Awake()
     {
         _rigid = GetComponent<Rigidbody2D>();
@@ -15,11 +25,36 @@ public class AgentMovement : MonoBehaviour
 
     void Update()
     {
-        
+
     }
 
     public void ChangeVelocity(Vector2 input)
     {
         _rigid.velocity = input.normalized * _speed;
+    }
+
+    public void FlipAgent(Vector2 mousePos)
+    {
+        _playerRenderer.flipX = transform.position.x > mousePos.x;
+    }
+
+    public void IsDash()
+    {
+        StartCoroutine(Dash());
+    }
+
+    private IEnumerator Dash()
+    {
+        GetComponent<CapsuleCollider2D>().enabled = false;
+        canDash = false;
+        isDashing = true;
+        //dashing
+        tr.emitting = true;
+        yield return new WaitForSeconds(dashTime);
+        tr.emitting = false;
+        isDashing = false;
+        GetComponent<CapsuleCollider2D>().enabled = true;
+        yield return new WaitForSeconds(dashCoolTime);
+        canDash = true;
     }
 }
--- Dungeon_BossRush/Assets/01_Scripts/Inheo/GunShooting.cs
+++ Dungeon_BossRush/Assets/01_Scripts/Inheo/GunShooting.cs
@@ -5,38 +5,31 @@ using UnityEngine;
 
 public class GunShooting : MonoBehaviour
 {
+    [SerializeField] private GameObject _player;
     [SerializeField] private GameObject _bullet;
     [SerializeField] private Transform _pos;
     [SerializeField] private float _coolTime;
 
     private float _curTime;
 
-    [SerializeField] private SpriteRenderer _renderer;
-
-    Vector3 flipMove = Vector3.zero;
+    [SerializeField] private SpriteRenderer _gunRenderer;
 
     void Update()
     {
-        Vector2 len = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
-        float z = Mathf.Atan2(len.y, len.x) * Mathf.Rad2Deg;
-        transform.rotation = Quaternion.Euler(0, 0, z);
-        if(z > 90 || z < -90)
-        {
-            flipMove = Vector3.left;
-            _renderer.flipY = true;
-        }
-        else if (z < 90 || z > -90)
-        {
-            flipMove = Vector3.right;
-            _renderer.flipY = false;
-        }
+    }
 
+    public void SetWeaponAngle(Vector2 mousePos)
+    {
+        Vector2 dir = mousePos - (Vector2)transform.position;
+        float angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.Euler(0, 0, angle);
+        _gunRenderer.flipY = transform.position.x > mousePos.x;
+    }
+    public void Shooting()
+    {
         if (_curTime <= 0)
         {
-            if (Input.GetMouseButton(0))
-            {
-                Instantiate(_bullet, _pos.position, transform.rotation);
-            }
+            Instantiate(_bullet, _pos.position, transform.rotation);
             _curTime = _coolTime;
         }
         _curTime -= Time.deltaTime;
